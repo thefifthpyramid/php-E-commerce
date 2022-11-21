5b649a482f9464996ab2efcf6ca5209d50ec6bbf
Ahmed Ali Klay <ahmedaliklay@outlook.com>
2022-11-21 12:07:20 +0200

    index test

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php include "includes/templates/header.php";  ?>
-
-//content
-<button class="btn btn-danger">click</button>
-<i class="fa fa-facebook"></i>
-
-<?php include "includes/templates/footer.php"; ?>
