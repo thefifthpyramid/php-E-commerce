315ff97bfea218c852f173ecb1e959ca3d903416
Ahmed Ali Klay <ahmedaliklay@outlook.com>
2022-11-26 06:08:41 +0200

    config file

--- Admin/conf.php
+++ Admin/conf.php
@@ -14,3 +14,30 @@
     catch (PDOException $e){
         echo "faild to connect " . $e->getMessage();
     }
+
+/*
+     * tables relation
+        ALTER TABLE items
+        ADD CONSTRAINT cat_1
+        FOREIGN KEY(cat_id)
+        REFERENCES categories(id)
+        ON UPDATE CASCADE;
+
+     *
+        SELECT items.*, categories.name AS cat_name,users.userName FROM items
+        INNER JOIN categories ON categories.id = items.cat_id
+        INNER JOIN users ON users.id = items.member_id
+
+
+     *  ALTER TABLE comments ADD CONSTRAINT items_comment FOREIGN KEY(item_id) REFERENCES items(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE;
+
+
+     *  ALTER TABLE comments ADD CONSTRAINT comment_user
+        FOREIGN KEY(user_id) REFERENCES users(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE;
+
+
+*/
