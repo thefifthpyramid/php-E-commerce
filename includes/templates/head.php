8065fd04532c225e4900486935bd99fbadf0b7b9
Ahmed Ali Klay <ahmedaliklay@outlook.com>
2022-11-30 17:48:08 +0200

    separet the head part

---
+++ includes/templates/head.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title><?php getTitle(); ?></title>
+
+    <!-- ::::::::::::::Favicon icon::::::::::::::-->
+    <link rel="shortcut icon" href="layout/assets/images/favicon.ico" type="image/png">
+    <!-- ::::::::::::::All CSS Files here :::::::::::::: -->
+    <!-- Vendor CSS -->
+    <link rel="stylesheet" href="layout/assets/css/vendor/font-awesome.min.css">
+    <link rel="stylesheet" href="layout/assets/css/vendor/ionicons.css">
+    <link rel="stylesheet" href="layout/assets/css/vendor/simple-line-icons.css">
+    <link rel="stylesheet" href="layout/assets/css/vendor/jquery-ui.min.css">
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="layout/assets/css/plugins/swiper-bundle.min.css">
+    <link rel="stylesheet" href="layout/assets/css/plugins/animate.min.css">
+    <link rel="stylesheet" href="layout/assets/css/plugins/nice-select.css">
+    <link rel="stylesheet" href="layout/assets/css/plugins/venobox.min.css">
+    <link rel="stylesheet" href="layout/assets/css/plugins/jquery.lineProgressbar.css">
+    <link rel="stylesheet" href="layout/assets/css/plugins/aos.min.css">
+
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="layout/assets/css/style.css">
+
+    <link rel="stylesheet" href="layout/assets/css/mystyle.css">
+    <!-- Use the minified version files listed below for better performance and remove the files listed above -->
+    <!-- <link rel="stylesheet" href="layout/assets/css/vendor/vendor.min.css">
+    <link rel="stylesheet" href="layout/assets/css/plugins/plugins.min.css">
+    <link rel="stylesheet" href="layout/assets/css/style.min.css"> -->
+
+</head>
+
+<body>
